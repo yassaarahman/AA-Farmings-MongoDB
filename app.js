59405dfab6feee4f051b5c73d6c152826fcb19e3
Muhammad Yassaa Rahman <yassaarahman@gmail.com>
2026-08-24 17:06:28 +0500

    Integrate session management with MongoDB for user authentication

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const path = require("path");
 // External Module
 const express = require("express");
 const { Result } = require("postcss");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const DB_PATH =
+  "mongodb://root:root@ac-wn1imgd-shard-00-00.dpdyonr.mongodb.net:27017,ac-wn1imgd-shard-00-01.dpdyonr.mongodb.net:27017,ac-wn1imgd-shard-00-02.dpdyonr.mongodb.net:27017/aa-farmings?ssl=true&replicaSet=atlas-7ecux2-shard-0&authSource=admin&appName=Cluster0";
 
 // Local Modules
 const { adminRouter } = require("./routes/adminRouter");
 const storeRouter = require("./routes/storeRouter");
+const authRouter = require("./routes/authRouter");
 const rootDir = require("./utils/pathUtil");
 const { default: mongoose } = require("mongoose");
 
@@ -20,17 +25,43 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+const store = new MongoDBStore({
+  uri: DB_PATH,
+  collection: "sessions",
+});
+
 app.use(express.urlencoded({ extended: true }));
+
+// Session Midleware
+app.use(
+  session({
+    secret: "AA Farmings",
+    resave: false,
+    saveUninitialized: true,
+    store, // is equal to "store: "store"
+  }),
+);
+// Session Middleware
+app.use((req, res, next) => {
+  req.isLoggedIn = req.session.isLoggedIn;
+  next();
+});
+
 app.use(express.json());
-app.use(adminRouter);
+app.use(authRouter);
+app.use("/admin", (req, res, next) => {
+  if (!req.isLoggedIn) {
+    return res.redirect("/login");
+  }
+  next();
+});
+app.use("/admin", adminRouter);
 app.use(storeRouter);
 app.use(express.static(path.join(rootDir, "public")));
 
 app.use(errorsController.pageNotFound);
 
 const PORT = 3000;
-const DB_PATH =
-  "mongodb://user:user@ac-wn1imgd-shard-00-00.dpdyonr.mongodb.net:27017,ac-wn1imgd-shard-00-01.dpdyonr.mongodb.net:27017,ac-wn1imgd-shard-00-02.dpdyonr.mongodb.net:27017/aa-farmings?ssl=true&replicaSet=atlas-7ecux2-shard-0&authSource=admin&appName=Cluster0";
 mongoose
   .connect(DB_PATH)
   .then(() => {
