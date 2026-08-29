ef1a60c01d87b206fa5a08b24c563b95c8163872
Muhammad Yassaa Rahman <yassaarahman@gmail.com>
2026-08-29 20:12:52 +0500

    Add user information to 404 error page rendering

--- controllers/404.js
+++ controllers/404.js
@@ -1,5 +1,9 @@
 exports.pageNotFound = (req, res, next) => {
   res
     .status(404)
-    .render("404", { pageTitle: "Page not found", isLoggedIn: req.isLoggedIn });
+    .render("404", {
+      pageTitle: "Page not found",
+      isLoggedIn: req.isLoggedIn,
+      user: req.session.user,
+    });
 };
